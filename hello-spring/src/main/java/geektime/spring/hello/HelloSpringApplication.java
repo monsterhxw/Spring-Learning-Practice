41413a4cf5305855671be4c2226a6fcab486d9e6
monsterhxw <770342643@qq.com>
2019-08-26 18:27:48 +0800

    fea(*): updated hello-spring project to learn Spring Framework.

--- hello-spring/src/main/java/geektime/spring/hello/HelloSpringApplication.java
+++ hello-spring/src/main/java/geektime/spring/hello/HelloSpringApplication.java
@@ -4,10 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
 public class HelloSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloSpringApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello Spring";
 	}
 
 }
