49590f6a54716e7c3b1f6a99dd493f8aaff857b5
monsterhxw <770342643@qq.com>
2019-09-04 01:44:05 +0800

    fea(*): added multiple practice project to learn Spring MVC.

---
+++ exception-springmvc/src/main/java/geektime/spring/controller/model/CoffeeOrder.java
@@ -0,0 +1,41 @@
+package geektime.spring.controller.model;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Enumerated;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OrderBy;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Table(name = "T_ORDER")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+public class CoffeeOrder extends BaseEntity implements Serializable {
+
+    private static final long serialVersionUID = -4968196127553351120L;
+
+    private String customer;
+
+    @ManyToMany
+    @JoinTable(name = "T_ORDER_COFFEE")
+    @OrderBy("id")
+    private List<Coffee> items;
+
+    @Enumerated
+    @Column(nullable = false)
+    private OrderState state;
+}
