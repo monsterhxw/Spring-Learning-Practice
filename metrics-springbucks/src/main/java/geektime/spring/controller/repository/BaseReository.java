d9040d873da5003dbf4d5c4798d20060639614af
monsterhxw <770342643@qq.com>
2019-09-06 00:34:01 +0800

    Revert "fea(property-source): added practice project to learn Spring Health Indicator and Meter."
    This reverts commit 8d5909749e5f554afc57f59e66d851657c16513f.

--- metrics-springbucks/src/main/java/geektime/spring/controller/repository/BaseReository.java
+++
@@ -1,9 +0,0 @@
-package geektime.spring.controller.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.NoRepositoryBean;
-
-@NoRepositoryBean
-public interface BaseReository<T, R> extends JpaRepository<T, R> {
-
-}
--- metrics-springbucks/src/main/java/geektime/spring/controller/repository/CoffeeOrderRepository.java
+++
@@ -1,7 +0,0 @@
-package geektime.spring.controller.repository;
-
-import geektime.spring.controller.model.CoffeeOrder;
-
-public interface CoffeeOrderRepository extends BaseReository<CoffeeOrder, Long> {
-
-}
