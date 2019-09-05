6fcd0994a706a7b6f9490ecf5175378c1d5c3920
monsterhxw <770342643@qq.com>
2019-09-06 00:35:11 +0800

    fea(*): added practice project to learn Spring Health Indicator and Meter.

---
+++ health-indicator-springbucks/src/main/java/geektime/spring/controller/service/CoffeeService.java
@@ -0,0 +1,20 @@
+package geektime.spring.controller.service;
+
+import geektime.spring.controller.model.Coffee;
+import java.util.List;
+import org.joda.money.Money;
+
+public interface CoffeeService {
+
+    List<Coffee> getAllCoffee();
+
+    List<Coffee> getCoffeeByName(List<String> names);
+
+    Coffee getCoffee(Long id);
+
+    Coffee getCoffee(String name);
+
+    Coffee saveCoffee(String name, Money price);
+
+    long getCoffeeCount();
+}
