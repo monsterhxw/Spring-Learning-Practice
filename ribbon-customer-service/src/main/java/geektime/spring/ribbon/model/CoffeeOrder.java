a4c1216fa9bff685ec67e3df6a45ee1bd873b00a
monsterhxw <770342643@qq.com>
2019-09-07 23:43:13 +0800

    fea(ribbon-customer-service): added practice project to learn Spring Cloud Load Balancer and Ribbon.

---
+++ ribbon-customer-service/src/main/java/geektime/spring/ribbon/model/CoffeeOrder.java
@@ -0,0 +1,25 @@
+package geektime.spring.ribbon.model;
+
+import java.util.Date;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CoffeeOrder {
+
+    private Long id;
+
+    private String customer;
+
+    private List<Coffee> items;
+
+    private OrderState state;
+
+    private Date createTime;
+
+    private Date updateTime;
+}
