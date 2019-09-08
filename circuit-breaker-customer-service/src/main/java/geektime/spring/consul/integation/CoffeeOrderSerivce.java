cd977ce72b56e0a0eb52b0c9fd1c585eabd44938
monsterhxw <770342643@qq.com>
2019-09-09 03:30:13 +0800

    fea(circuit-breaker-customer-service): added practice project to learn Spring Cloud and Custom Circuit Breaker By AOP.

---
+++ circuit-breaker-customer-service/src/main/java/geektime/spring/consul/integation/CoffeeOrderSerivce.java
@@ -0,0 +1,21 @@
+package geektime.spring.consul.integation;
+
+import geektime.spring.consul.model.CoffeeOrder;
+import geektime.spring.consul.model.NewOrderRequest;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "springbucks-service", contextId = "coffeeOrder")
+public interface CoffeeOrderSerivce {
+
+    @GetMapping("/order/{id}")
+    CoffeeOrder getOrder(@PathVariable("id") Long id);
+
+    @PostMapping(path = "/order", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    CoffeeOrder create(@RequestBody NewOrderRequest newOrder);
+
+}
