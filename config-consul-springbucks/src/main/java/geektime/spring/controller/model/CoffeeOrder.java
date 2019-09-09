23f3631f1d7b39f76e57aa9167532cea55ae2bac
monsterhxw <770342643@qq.com>
2019-09-10 02:47:51 +0800

    fea(config-consul-springbucks): added practice project to learn Spring Cloud and Consul Config.

---
+++ config-consul-springbucks/src/main/java/geektime/spring/controller/model/CoffeeOrder.java
@@ -0,0 +1,51 @@
+package geektime.spring.controller.model;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Enumerated;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OrderBy;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.Type;
+import org.joda.money.Money;
+
+@Entity
+@Table(name = "T_ORDER")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+public class CoffeeOrder extends BaseEntity implements Serializable {
+
+    private static final long serialVersionUID = -4968196127553351120L;
+
+    private String customer;
+
+    @ManyToMany
+    @JoinTable(name = "T_ORDER_COFFEE")
+    @OrderBy("id")
+    private List<Coffee> items;
+
+    @Enumerated
+    @Column(nullable = false)
+    private OrderState state;
+
+    private Integer discount;
+
+    @Type(type = "org.jadira.usertype.moneyandcurrency.joda.PersistentMoneyMinorAmount",
+        parameters = {@org.hibernate.annotations.Parameter(name = "currencyCode", value = "CNY")})
+    private Money total;
+
+    private String waiter;
+}
